8160e2898eaa25beeb1e4553b85c888fbafd385e
intrening <wol87@ya.ru>
2019-07-11 10:05:24 +0300

    fixed tabs

--- script.py
+++ script.py
@@ -2,14 +2,14 @@ import random
 from datacenter.models import *
 
 def fix_marks(schoolkid):
-	marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2,3])
-	for mark in marks:
-		mark.points = 5
-		mark.save()
-		
+    marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2,3])
+    for mark in marks:
+        mark.points = 5
+        mark.save()
+
 def remove_chastisements(schoolkid):
-	chastisements = Сhastisement.objects.filter(schoolkid=schoolkid)
-	chastisements.delete()
+    chastisements = Сhastisement.objects.filter(schoolkid=schoolkid)
+    chastisements.delete()
 
 def add_random_commendation(schoolkid, subject_name):
     commendation_list = ['Молодец!','Хорошо!','Отлично!','Гораздо лучше, чем я ожидал!','Ты меня приятно удивил!','Великолепно!','Прекрасно!','Ты меня очень обрадовал!','Именно этого я давно ждал от тебя!','Сказано здорово – просто и ясно!','Ты, как всегда, точен!','Очень хороший ответ!','Талантливо!','Ты сегодня прыгнул выше головы!','Я поражен!','Уже существенно лучше!','Потрясающе!','Замечательно!','Прекрасное начало!','Так держать!','Ты на верном пути!','Здорово!','Это как раз то, что нужно!','Я тобой горжусь!','С каждым разом у тебя получается всё лучше!','Мы с тобой не зря поработали!','Я вижу, как ты стараешься!','Ты растешь над собой!','Ты многое сделал, я это вижу!','Теперь у тебя точно все получится!']
